5e2fdd34a4625f437ca83f590e4433b0eb4ec051
Grisha Petrov <g.petrov@artsofte.ru>
2021-10-21 18:39:42 +0500

    start project

--- get-offers/src/app/app.component.ts
+++ get-offers/src/app/app.component.ts
@@ -3,20 +3,11 @@ import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  // constructor(private _httpClient: HttpClient) {
 
-  // }
-
-  // public ngOnInit(): void {
-  //   this._httpClient.get('http://localhost:808/api/classificator').subscribe( vl => {
-  //     console.log(vl);
-  //   })
-  // }
-  
 }
--- get-offers/src/app/app.module.ts
+++ get-offers/src/app/app.module.ts
@@ -4,30 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderMenuComponent } from './header-menu/header-menu.component';
-import { GroupsComponent } from './groups/groups.component';
-import { TableComponent } from './table/table.component';
-import { MaterialModule } from './material/material.module';
-import { ItemsListComponent } from './items-list/items-list.component';
-import { ButtonGetdataComponent } from './button-getdata/button-getdata.component';
 import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderMenuComponent,
-    GroupsComponent,
-    TableComponent,
-    ItemsListComponent,
-    ButtonGetdataComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
     HttpClientModule
   ],
   providers: [],
